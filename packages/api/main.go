0a17a30b999d8406e6ba558e715a824aed99264d
TheSw1m <swim853279614@163.com>
2025-12-24 01:08:20 +0800

    feat: add sign api

--- packages/api/main.go
+++ packages/api/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -28,11 +30,30 @@ type StrShelfResponse[T any] struct {
 	Msg  string `json:"msg"`
 }
 
+type PostRequestResponse struct {
+	Code   uint16 `json:"code"`
+	Result any    `json:"result"`
+}
+
+type UserInfo struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 type CustomTime time.Time
 
 func (ct CustomTime) MarshalJSON() ([]byte, error) {
 	t := time.Time(ct)
 	return json.Marshal(t.UnixMilli())
+}
+func (ct *CustomTime) UnmarshalJSON(data []byte) error {
+
+	var ms int64
+	if err := json.Unmarshal(data, &ms); err == nil {
+		*ct = CustomTime(time.Unix(0, ms*int64(time.Millisecond)))
+		return nil
+	} else {
+		return fmt.Errorf("cannot unmarshal %s into CustomTime: %w", string(data), err)
+	}
 }
 
 func main() {
@@ -59,7 +80,60 @@ func main() {
 			Msg:  "",
 		})
 	})
+	r.POST("/v1/item.post", func(ctx *gin.Context) {
+		newShelfItem := ShelfItem{}
+		result := gorm.WithResult()
+		err := ctx.ShouldBindJSON(&newShelfItem)
+		if err != nil {
+			ctx.JSON(500, "error in binding json: "+err.Error())
+			fmt.Println(err.Error())
+			return
+		}
+
+		err = gorm.G[any](db).Exec(context.Background(), "INSERT INTO public.shelf_item_v1 (title,link,comment,deleted) VALUES(?,?,?,?)", newShelfItem.Title, newShelfItem.Link, newShelfItem.Comment, false)
+
+		if err != nil {
+			ctx.JSON(500, "error in insert database: "+err.Error())
+			fmt.Println(err.Error())
+			return
+		}
+		ctx.JSON(200, PostRequestResponse{
+			Code:   200,
+			Result: result,
+		})
+	})
+	r.POST("/v1/user.signup", func(ctx *gin.Context) {
+		newUser := UserInfo{}
+		result := gorm.WithResult()
+		err := ctx.BindJSON(&newUser)
+		if err != nil {
+			ctx.JSON(500, "internal error")
+			return
+		}
+		HashedPassword, err := HashPassword(newUser.Password)
+		if err != nil {
+			ctx.JSON(500, "internal error")
+			return
+		}
+
+		err = gorm.G[any](db).Exec(context.Background(), "INSERT INTO public.shelf_user_v1 (username,password) VALUES(?,?)", newUser.Username, HashedPassword)
+		if err != nil {
+			ctx.JSON(500, "error in insert database: "+err.Error())
+			fmt.Println(err.Error())
+			return
+		}
+		ctx.JSON(200, PostRequestResponse{
+			Code:   200,
+			Result: result,
+		})
+
+	})
 
 	r.Run(":1111")
+
+}
+
+func HashPassword(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 }
