eab905be29e6d9e73aa2a7f28af99f14f5d7c5bc
TheSw1m <swim853279614@163.com>
2026-02-11 11:41:01 +0800

    fix: correct config load and display which config was loaded

--- packages/api/config/config.go
+++ packages/api/config/config.go
@@ -5,27 +5,21 @@ import (
 	"gopkg.ilharper.com/strshelf/api/logger"
 )
 
-type Config struct {
-	Port int `json:"port"`
-}
-
 var ViperInstance *viper.Viper
-var ConfigInstance Config
 
 func InitConfig() {
-	ViperInstance = viper.New()
-	v := ViperInstance
-	v.AddConfigPath("packages/api/config/")
-	v.AddConfigPath("./config/")
-	v.AddConfigPath("./")
-	v.AddConfigPath(".")
-	v.SetConfigName("config.json")
-	v.SetConfigType("json")
-	err := v.ReadInConfig()
+	viper.AddConfigPath("packages/api/config/")
+	viper.AddConfigPath("./config/")
+	viper.AddConfigPath("./")
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config.json")
+	viper.SetConfigType("json")
+	viper.WatchConfig()
+	err := viper.ReadInConfig()
 	if err != nil {
 		logger.Suger.Errorf("can not load config: %s", err.Error())
 	} else {
-		logger.Suger.Infof("config load successful")
-		ConfigInstance.Port = v.GetInt("port")
+		logger.Suger.Infof("config load successful: %s", viper.ConfigFileUsed())
+		logger.Suger.Debugln(viper.AllKeys())
 	}
 }
